55265340358aa66160ecb22882c47b3da7b566d1
sealx017 <48604757+sealx017@users.noreply.github.com>
2026-09-01 08:18:27 -0400

    Refactor file paths and enhance comments
    Updated file paths and improved comments in Wine_example.py.

--- Wine_example.py
+++ Wine_example.py
@@ -1,4 +1,4 @@
-                    #!/usr/bin/env python3
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
 Created on Fri May 10 10:35:37 2024
@@ -9,11 +9,12 @@ import pandas as pd   # primarily used for data wrangling, R eqv. data.frame and
 import numpy as np    # basic linear algebra like matrix product, inverse etc.
 import seaborn as sns # for plotting, ggplot-wannabe
 import scipy as sp    # offers many statisitcal functions
-import math as math   # basic math functions
-import spm1d as spm1d # some multivariate statistics
+import math as math   # offeres math operator functions
+from scipy.linalg import solve_triangular # use "from" "import" to load specific functions
+
 
 # pandas data reading
-X = pd.read_csv('/Users/sealso/Research/Teaching/Data/wine.data.txt', sep=",", header=None)
+X = pd.read_csv('/Users/sealso/Research/Teaching/BMTRY_783_2026/Data/wine.data.txt', sep=",", header=None)
 X.shape
 
 # There is one row per wine sample. The first column contains the cultivar of a wine sample (labelled 1, 2 or 3), 
@@ -38,22 +39,22 @@ X.columns = column_names
 # pairwise plot, ggally package in R does the same
 
 sns_plot1 = sns.pairplot(X.loc[:, 'chemical_1':'chemical_5'])
-sns_plot1.figure.savefig("/Users/sealso/Research/Teaching/Codes/Results/out1.png")
+sns_plot1.figure.savefig("/Users/sealso/Research/Teaching/BMTRY_783_2026/Codes/Results/out1.png")
 
 [sp.stats.pearsonr(X.loc[:, 'chemical_1'], X.loc[:, y]) for y in column_names[2:14]] # pearson r
 [sp.stats.spearmanr(X.loc[:, 'chemical_1'], X.loc[:, y]) for y in column_names[2:14]] # spearman r
 
 sns_plot2 = sns.pairplot(X.loc[:, 'cultivar':'chemical_5'], hue="cultivar", diag_kind="hist")
-sns_plot2.figure.savefig("/Users/sealso/Research/Teaching/Codes/Results/out2.png")
+sns_plot2.figure.savefig("/Users/sealso/Research/Teaching/BMTRY_783_2026/Codes/Results/out2.png")
 
 sns_plot3 = sns.pairplot(X.loc[:, 'chemical_1':'chemical_5'], kind="hist")
-sns_plot3.figure.savefig("/Users/sealso/Research/Teaching/Codes/Results/out3.png")
+sns_plot3.figure.savefig("//Users/sealso/Research/Teaching/BMTRY_783_2026/Codes/Results/out3.png")
 
 sns_plot4 = sns.pairplot(X.loc[:, 'chemical_1':'chemical_5'], kind="kde")
-sns_plot4.figure.savefig("/Users/sealso/Research/Teaching/Codes/Results/out4.png")
+sns_plot4.figure.savefig("/Users/sealso/Research/Teaching/BMTRY_783_2026/Codes/Results/out4.png")
 
 sns_plot5 = sns.pairplot(X.loc[:, 'cultivar':'chemical_5'], hue="cultivar", kind="kde")
-sns_plot5.figure.savefig("/Users/sealso/Research/Teaching/Codes/Results/out5.png")
+sns_plot5.figure.savefig("/Users/sealso/Research/Teaching/BMTRY_783_2026/Codes/Results/out5.png")
 
 
 
@@ -165,15 +166,82 @@ def MVN_sigma(Sigma_vec):
 cov_1_vec = np.append(np.diag(cov_1), cov_1[np.tril_indices(p, -1)])
 MVN_sigma(cov_1_vec)
 
-cov_0 = np.zeros([p, p])
-np.fill_diagonal(cov_0, 1)
+cov_0 = np.eye(p, p)
 cov_0_vec = np.append(np.diag(cov_0), cov_0[np.tril_indices(p, -1)])
 
 initial_guess = cov_0_vec
 MVN_sigma(initial_guess)
-#bnds = tuple((0,math.inf) for x in initial_guess[range(p)]) + tuple((-math.inf,math.inf) for x in initial_guess[range((p), len(initial_guess))])
-#result_sigma = sp.optimize.minimize(MVN_sigma, initial_guess,  method='SLSQP', bounds=bnds)
-result_sigma = sp.optimize.minimize(MVN_sigma, initial_guess,  method='L-BFGS-B', options={'maxiter': 500})
+
+result_sigma = sp.optimize.minimize(MVN_sigma, initial_guess,  
+               method='L-BFGS-B', options={'maxiter': 500})
+
+result_sigma
+
+##########
+# retry
+##########
+
+residual = data - mu
+off_diag = np.tril_indices(p, -1)
+
+def theta_to_cholesky(theta):
+    L = np.zeros((p, p))
+
+    # Exponentiation guarantees positive diagonal elements
+    L[np.diag_indices(p)] = np.exp(theta[:p])
+    L[off_diag] = theta[p:]
+
+    return L
+
+def MVN_sigma(theta):
+    L = theta_to_cholesky(theta)
+
+    # Solve L z_i = x_i - mu without explicitly inverting Sigma
+    Z = solve_triangular(
+        L,
+        residual.T,
+        lower=True,
+        check_finite=False
+    )
+
+    quadratic_term = np.sum(Z**2)
+    log_determinant = 2 * np.sum(np.log(np.diag(L)))
+
+    return 0.5 * (
+        quadratic_term +
+        n * log_determinant
+    )
+
+Sigma_initial = cov_0
+
+# Small stabilization in case Sigma_initial is nearly singular
+scale = max(np.trace(Sigma_initial) / p, 1.0)
+Sigma_initial += 1e-8 * scale * np.eye(p)
+
+L_initial = np.linalg.cholesky(Sigma_initial)
+
+initial_theta = np.concatenate([
+    np.log(np.diag(L_initial)),
+    L_initial[off_diag]
+])
+
+result_sigma = sp.optimize.minimize(
+    MVN_sigma,
+    initial_theta,
+    method="L-BFGS-B",
+    options={
+        "maxiter": 500,
+        "maxls": 50,
+        "gtol": 1e-7
+    }
+)
+
+L_hat = theta_to_cholesky(result_sigma.x)
+Sigma_hat_optimized = L_hat @ L_hat.T
+
+print(result_sigma)
+print(Sigma_hat_optimized)
+print(np.linalg.eigvalsh(Sigma_hat_optimized))
 
 
 Sigma_MLE_vec = result_sigma.x
@@ -182,7 +250,8 @@ Sigma_MLE[np.tril_indices(p, -1)] = Sigma_MLE_vec[range(p, len(Sigma_MLE_vec))]
 Sigma_MLE = (Sigma_MLE + Sigma_MLE.T)
 np.fill_diagonal(Sigma_MLE, Sigma_MLE_vec[range(p)])
 Sigma_MLE - cov_1
-    
+
+
 
 # Hotelling T2 
 
@@ -205,12 +274,16 @@ p = cultivar2.shape[1]
 W1 = (n1 - 1)*cov_1
 W2 = (n2 - 1)*cov_2
 S_pl = 1/(n1 + n2 - 2)*(W1 + W2)
-T2 = n1*n2/(n1 + n2)*np.dot((mean_1 - mean_2).T, np.dot(np.linalg.inv(S_pl), (mean_1-mean_2)))
+T2 = n1*n2/(n1 + n2)*np.dot((mean_1 - mean_2).T, 
+           np.dot(np.linalg.inv(S_pl), (mean_1-mean_2)))
 T2_star = (n1 + n2 - p - 1)/(n1 + n2 - 2)/p*T2
 F_val = sp.stats.f.sf(T2_star, p, (n1 + n2 - p - 1)) # sp.stats.f.ppf(0.95, p, (n1 + n2 - p - 1))
 
 # same statistic using existing package 
-
+import spm1d as spm1d 
 T2    = spm1d.stats.hotellings2(cultivar1, cultivar2)
 T2i   = T2.inference(0.05)
 T2i
+
+
+
